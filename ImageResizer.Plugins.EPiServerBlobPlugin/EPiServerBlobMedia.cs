cc5a9c984ba707e279ffa72d1d4b2e2eb2c6f4c3
Igelkottegrodan <andre.hedberg@gmail.com>
2015-12-21 12:46:25 +0100

    Initial commit.

---
+++ ImageResizer.Plugins.EPiServerBlobPlugin/EPiServerBlobMedia.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using EPiServer.Core;
+using EPiServer.Framework.Blobs;
+using EPiServer.ServiceLocation;
+using EPiServer.Web.Routing;
+
+namespace ImageResizer.Plugins.EPiServerBlob
+{
+    public class EPiServerBlobMedia : IVirtualFileWithModifiedDate
+    {
+        private static readonly DateTime __minUniversalDateTime = DateTime.MinValue.ToUniversalTime();
+        private Blob _blob;
+        private MediaData _mediaData;
+        protected readonly ContentRouteHelper ContentRouteHelper;
+
+        public EPiServerBlobMedia(string virtualPath)
+            : this(virtualPath, ServiceLocator.Current.GetInstance<ContentRouteHelper>())
+        {
+
+        }
+
+        public EPiServerBlobMedia(string virtualPath, ContentRouteHelper contentRouteHelper)
+        {
+            this.ContentRouteHelper = contentRouteHelper;
+            this.VirtualPath = virtualPath;
+        }
+
+        public Blob Blob
+        {
+            get
+            {
+                if (this._blob != null)
+                {
+                    return this._blob;
+                }
+
+                if (this.MediaData == null)
+                {
+                    return null;
+                }
+
+                var binaryStorable = MediaData as IBinaryStorable;
+                if (binaryStorable == null || binaryStorable.BinaryData == null)
+                {
+                    return null;
+                }
+
+                this._blob = binaryStorable.BinaryData;
+
+                return this._blob;
+            }
+        }
+
+        public MediaData MediaData
+        {
+            get
+            {
+                if (this._mediaData != null)
+                {
+                    return this._mediaData;
+                }
+
+                this._mediaData = this.ContentRouteHelper.Content as MediaData;
+
+                return this._mediaData;
+            }
+        }
+
+        public bool Exists
+        {
+            get { return (this.MediaData != null && this.MediaData.BinaryData != null); }
+        }
+
+        public Stream Open()
+        {
+            if (this.Blob != null)
+            {
+                return this.Blob.OpenRead();
+            }
+
+            return null;
+        }
+
+        public string VirtualPath { get; private set; }
+
+        public DateTime ModifiedDateUTC
+        {
+            get
+            {
+                if (this.MediaData == null)
+                {
+                    return __minUniversalDateTime;
+                }
+
+                var changeTrackable = this.MediaData as IChangeTrackable;
+                if (changeTrackable == null)
+                {
+                    return __minUniversalDateTime;
+                }
+
+                return changeTrackable.Changed.ToUniversalTime();
+            }
+        }
+    }
+}
---
+++ ImageResizer.Plugins.EPiServerBlobPlugin/EPiServerBlobPlugin.cs
@@ -0,0 +1,94 @@
+﻿using EPiServer;
+using EPiServer.Core;
+using EPiServer.ServiceLocation;
+using EPiServer.Web;
+using EPiServer.Web.Routing;
+using EPiServer.Web.Routing.Segments;
+using ImageResizer.Configuration;
+using System.Collections.Specialized;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace ImageResizer.Plugins.EPiServerBlob
+{
+    public class EPiServerImageBlobPlugin : IVirtualImageProvider, IPlugin
+    {
+        private readonly UrlResolver __urlResolver = ServiceLocator.Current.GetInstance<UrlResolver>();
+
+        public bool FileExists(string virtualPath, NameValueCollection queryString)
+        {
+            EPiServerBlobMedia blobImage = this.GetBlobFile(virtualPath);
+
+            return (blobImage != null && blobImage.Exists);
+        }
+
+        public IVirtualFile GetFile(string virtualPath, NameValueCollection queryString)
+        {
+            return this.GetBlobFile(virtualPath);
+        }
+
+        private EPiServerBlobMedia GetBlobFile(string virtualPath)
+        {
+            EPiServerBlobMedia blobFile = new EPiServerBlobMedia(virtualPath);
+
+            return blobFile;
+        }
+
+        public IPlugin Install(Config config)
+        {
+            config.Plugins.add_plugin(this);
+            config.Pipeline.PostAuthorizeRequestStart += OnPostAuthorizeRequestStart;
+
+            return this;
+        }
+
+        public bool Uninstall(Config config)
+        {
+            config.Plugins.remove_plugin(this);
+            config.Pipeline.PostAuthorizeRequestStart -= OnPostAuthorizeRequestStart;
+
+            return true;
+        }
+
+        private void OnPostAuthorizeRequestStart(IHttpModule sender, HttpContext context)
+        {
+            string absolutePath = context.Request.Url.AbsolutePath;
+            IContent resolvedContent = __urlResolver.Route(new UrlBuilder(absolutePath), context.GetRequestContext().GetContextMode());
+
+            if (resolvedContent == null)
+            {                
+                return;
+            }
+
+            bool isMediaContent = resolvedContent is MediaData;
+
+            if (!isMediaContent)
+            {
+                return;
+            }
+
+            Config.Current.Pipeline.SkipFileTypeCheck = true;
+
+            bool previewOrEditMode = (
+                RequestSegmentContext.CurrentContextMode == ContextMode.Edit ||
+                RequestSegmentContext.CurrentContextMode == ContextMode.Preview
+            );
+
+            // Disable cache if editing or previewing
+            if (previewOrEditMode)
+            {
+                Config.Current.Pipeline.PreRewritePath = this.CleanEditModePath(absolutePath);
+                NameValueCollection modifiedQueryString = new NameValueCollection(Config.Current.Pipeline.ModifiedQueryString);
+
+                modifiedQueryString.Add("process", ProcessWhen.Always.ToString());
+                modifiedQueryString.Add("cache", ServerCacheMode.No.ToString());
+                Config.Current.Pipeline.ModifiedQueryString = modifiedQueryString;
+            }                    
+        }
+
+        private string CleanEditModePath(string path)
+        {
+            return Regex.Replace(path, @",.*$", string.Empty);
+        }
+    }
+}
